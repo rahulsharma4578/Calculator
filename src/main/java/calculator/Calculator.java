0f918d31415db7d5ff8fa6cf1f9cfde84d369c58
rahulsharma <sharmarahul020899@gmail.com>
2023-03-22 17:37:23 +0530

    Added ngrok configurations lat

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -32,7 +32,7 @@ public class Calculator {
                             "Enter 3 to find Power\n" +
                             "Enter 4 to find Natural Logarithm\n" +
                             "Enter any other input to Exit\n" +
-                            "daddd Enter your choice: "
+                            "Enter your choice: "
             );
 
             int choice;
